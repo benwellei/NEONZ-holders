c99dcb108aaccb5adecc6a876cbc7653ac6a5b1b
benwellei <benwellei@protonmail.com>
2021-12-25 08:24:41 -0800

    Rename logs folder

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ import fetch from 'node-fetch';
 import * as fs from 'fs';
 
 function getNeonzHolders() {
-    if (!fs.existsSync('./log')) {
-        fs.mkdirSync('./log');
+    const header = ['Address', 'Token ID'];
+    const today = new Date();
+    const path = `./logs/holders.csv`;
+
+    if (!fs.existsSync('./logs')) {
+        fs.mkdirSync('./logs');
         console.log('Logs directory created.');
     }
-
-    const header = ['Address', 'Token ID'];
-    const path = './log/holders.csv';
-
-    fs.writeFile(path, `${header.join()}\n`, (err) => {
+    
+    fs.writeFileSync(path, `${header.join()}\n`, (err) => {
         if (err) throw err;
-    });
+    });   
 
     fetch('https://api.tzkt.io/v1/contracts/KT1MsdyBSAMQwzvDH4jt2mxUKJvBSWZuPoRJ/bigmaps/ledger/keys?limit=10000')
         .then(res => res.json())
